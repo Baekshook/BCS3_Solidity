8de8df11e1104223c733b99f59b02e6da364f88c
Baekshook <ecgo0106@naver.com>
2023-05-04 15:09:03 +0900

    ✨ : local variable, array - 넣기, 빼기, 보기, 바꾸기, 길이, delete, 전체반환

---
+++ Daily/0504_3.sol
@@ -0,0 +1,123 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.18;
+
+
+contract StringAndBytes {
+    string a;
+
+    function setString(string memory _a) public {
+        a = _a;
+    }
+
+    function getString() public view returns(string memory) {
+        return a;
+    }
+
+    function stringToBytes() public view returns(bytes memory){
+        return bytes(a);
+    }
+
+    function stringToBytes2() public view returns(bytes1, bytes1, bytes1) {
+        return (bytes(a)[0], bytes(a)[1], bytes(a)[2]);
+    }
+
+    function bytesToString(bytes memory _a) public pure returns(string memory) {
+        return string(_a);
+    }
+
+    //WIP
+    function bytesToString2(string memory _a) public pure returns(string memory) {
+        return string(_a);
+    } 
+}
+
+contract LocalVariable {
+    function A() public pure returns(uint) {
+        uint a=5;
+        uint b=7;
+        return (a+b);
+    }
+}
+
+
+contract Array {
+    // array = 같은 자료형들이 들어있는 집합
+    uint[] numbers; // 자료형 [] array 이름
+
+    // 1. 넣기
+    function push(uint _a) public {
+        numbers.push(_a); // 배열이름.push(변수)
+    }
+
+    // 2. 빼기
+    function pop() public {
+        numbers.pop(); // 배열이름.pop(), 맨뒤의 숫자를 날림
+    }
+
+    // 3. 보기
+    function getNumber(uint _n) public view returns(uint) {
+        return numbers[_n-1]; // 배열이름[_n번째]
+    }
+
+    // 4. 길이
+    function getLength() public view returns(uint) {
+        return numbers.length; //배열이름.길이
+    }
+
+    // 5. 바꾸기
+    function changeNum(uint _k, uint _z) public {
+        numbers[_k-1] = _z; //배열이름[_k번째] = _z -> _k번째를 _z로 바꿔
+    }
+
+    // 6. delete 
+    function deleteNum(uint _n) public {
+        delete numbers[_n-1]; // delete 배열이름[_n번째] = _n번째 숫자를 없애줘
+    }
+
+    //7. 전체 array 반환
+    function returnArray() public view returns(uint[] memory) {
+        return numbers;
+    }
+}
+
+contract Array_s {
+    // array = 같은 자료형들이 들어있는 집합
+    string[] letters; // 자료형 [] array 이름
+
+    // 1. 넣기
+    function push(string memory _a) public {
+        letters.push(_a); // 배열이름.push(변수)
+    }
+
+    // 2. 빼기
+    function pop() public {
+        letters.pop(); // 배열이름.pop(), 맨뒤의 숫자를 날림
+    }
+
+    // 3. 보기
+    function getLetter(uint _n) public view returns(string memory) {
+        return letters[_n-1]; // 배열이름[_n번째]
+    }
+
+    // 4. 길이
+    function getLength() public view returns(uint) {
+        return letters.length; //배열이름.길이
+    }
+
+    // 5. 바꾸기
+    function changeLet(uint _k, string memory _z) public {
+        letters[_k-1] = _z; //배열이름[_k번째] = _z -> _k번째를 _z로 바꿔
+    }
+
+    // 6. delete 
+    function deleteLet(uint _n) public {
+        delete letters[_n-1]; // delete 배열이름[_n번째] = _n번째 숫자를 없애줘
+    }
+
+    //7. 전체 array 반환
+    function returnArray() public view returns(string[] memory) {
+        return letters;
+    }
+    // bytes 뒤에 숫자 붙어있는 것은 자릿수가 정해져있기 때문에 memory 안붙여도 됨.
+}
